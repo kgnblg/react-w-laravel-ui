48e82c7baa721693d26da0ab6ff1bf1cdfc0e939
Kagan Balga <kgnblg@gmail.com>
2020-04-02 11:53:05 +0200

    dashboard structure updates

--- resources/js/components/dashboard/dashboard.js
+++ resources/js/components/dashboard/dashboard.js
@@ -17,15 +17,70 @@ class Dashboard extends React.Component {
         return (
             <div className="container py-4">
                 <div className="row justify-content-center">
-                    <div className="col-md-12">
-                            <div className="card">
-                                <div className="card-header">
-                                    Dashboard
-                                </div>
-                                <div className="card-body">
+                    <div className="col-md-12 mb-4">
+                        <div className="card">
+                            <div className="card-header">
+                                Dashboard
+                            </div>
+                            <div className="card-body">
+                                Larasix is an e-commerce application developed with React, Redux, Redux-Thunk and Laravel 6.
+                            </div>
+                        </div>
+                    </div>    
+                </div>
+
+                <div className="row justify-content-center">
+                    <div className="col-md-3">
+                        <div className="card">
+                            <div className="card-header">
+                                Products
+                            </div>
+                            <div className="card-body">
+                                <div align="center">
+                                    <h1>0</h1>
                                 </div>
                             </div>
-                        </div>    
+                        </div>
+                    </div>
+
+                    <div className="col-md-3">
+                        <div className="card">
+                            <div className="card-header">
+                                Orders
+                            </div>
+                            <div className="card-body">
+                                <div align="center">
+                                    <h1>0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+
+                    <div className="col-md-3">
+                        <div className="card">
+                            <div className="card-header">
+                                Returns
+                            </div>
+                            <div className="card-body">
+                                <div align="center">
+                                    <h1>0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
+
+                    <div className="col-md-3">
+                        <div className="card">
+                            <div className="card-header">
+                                Total Endorsement
+                            </div>
+                            <div className="card-body">
+                                <div align="center">
+                                    <h1>0.00</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  
                 </div>
             </div>
         )
