5cc0bca37f497b2c5ed7a254cc65ae15ae974a91
Kagan Balga <kgnblg@gmail.com>
2020-04-05 11:20:01 +0200

    new product list component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
 import { Provider } from "react-redux"
 import { store } from '../store/store'
 
 import Header    from './navbar/Header'
-import Footer    from './footer/footer'
+import Footer    from './footer/Footer'
 import Login     from './login/login'
 import Dashboard from './dashboard/dashboard'
+
+// Product page
+import ProductList from './products/productList'
 
 class App extends Component {
     render () {
@@ -17,8 +21,9 @@ class App extends Component {
                     <div>
                         <Header />
                         <Switch>
-                            <Route exact path='/'          component={Login}     />
-                            <Route exact path='/dashboard' component={Dashboard} />
+                            <Route exact path='/'          component={Login}       />
+                            <PrivateRoute exact path='/dashboard' component={Dashboard}   />
+                            <PrivateRoute exact path='/products'  component={ProductList} />
                         </Switch>
                         <Footer />
                     </div>
